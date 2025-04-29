35c6d37cda207b6a9a30fac01e55a5158e70872e
sunguti-albright <sungutialbright@gmail.com>
2025-04-29 12:30:50 +0300

    checker

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 USE alx_book_store;
-INSERT INTO Customers(customer_id, customer_name, email, address) VALUES(1, "Cole Baidoo", "cbaidoo@gmail.com", "123 Happiness Avenue");
+INSERT INTO customer(customer_id, customer_name, email, address) VALUES(1, "Cole Baidoo", "cbaidoo@gmail.com", "123 Happiness Avenue");
