bf4dd3c3d67ba4ba020fa4671f7525d9bbb6be23
sunguti-albright <sungutialbright@gmail.com>
2025-10-21 11:09:47 +0300

    create orders and order details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -42,3 +42,29 @@ CREATE TABLE IF NOT EXISTS Customers(
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT 
 );
+/*Orders: Stores information about orders placed by customers.
+order_id (Primary Key)
+customer_id (Foreign Key referencing Customers table)
+order_date DATE
+*/
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+/*
+Order_Details: Stores information about the books included in each order.
+orderdetailid (Primary Key)
+order_id (Foreign Key referencing Orders table)
+book_id (Foreign Key referencing Books table)
+quantity DOUBLE
+*/
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
