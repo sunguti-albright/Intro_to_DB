8c12566285bf8a4306032d5006a20a5997228d79
sunguti-albright <sungutialbright@gmail.com>
2025-11-04 11:08:28 +0300

    create customers  table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,3 +30,15 @@ CREATE TABLE IF NOT EXISTS Books(
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
+/*Customers: Stores information about customers.
+customer_id (Primary Key)
+customer_name VARCHAR(215)
+email VARCHAR(215)
+address TEXT
+*/
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT 
+);
