64a62f8aebc833be77cbbf2274f1207f27eb0232
sunguti-albright <sungutialbright@gmail.com>
2025-11-17 10:04:31 +0300

    a script that prints the full description of the table

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
+
+-- The database name will be passed as an argument of the mysql command
+USE alx_book_store;
+SELECT * FROM Books;
